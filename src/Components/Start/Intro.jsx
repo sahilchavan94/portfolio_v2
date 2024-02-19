554d008989edd8783ac0a7af0425d38f5bd43571
sahil-gpm <sahilchavangithub@gmail.com>
2024-02-19 18:21:24 +0530

    profile changes : mash burned dead

--- src/Components/Start/Intro.jsx
+++ src/Components/Start/Intro.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import demo from "../assets/demo.jpg";
+import demo from "../assets/demo.jpeg";
 import { useInView } from "react-intersection-observer";
 import Typing from "../Typed/Typing";
 import blob1 from "../assets/svgs/blob.svg";
